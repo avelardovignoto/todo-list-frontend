fa3f37f1fe78cde739e9652d1342a17fa2cde7f9
Avelardo Vignoto <avelar.vignoto@gmail.com>
2022-09-06 20:41:14 -0300

    Organizando o método fetch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#form");
 
 const sendTask = task => {
-    const url = 'http://localhost8080.api/v1/tasks';
+    const url = 'http://localhost:8080/api/v1/tasks';
     const header = {
         method: 'POST',
         headers: {"Content-Type": "application/json"},
@@ -9,18 +9,15 @@ const sendTask = task => {
     }
     fetch(url,header)
     .then(response => response.json)
-    .then(data => response);
-    const resp = data;
-    console.log(resp);
+    console.log(.then(response => response);
+    );
 }
-promessa();
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     const task = Object.create(null);
     task.nameTask = e.target.NameTask.value;
-    console.log(task);
 
     sendTask(task);
 })
